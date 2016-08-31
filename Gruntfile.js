57b639dc207bc5b5c296d71534270028a0705c88
Samuel <csamuel@SC07KP1B8DY3H.local>
2016-08-31 11:33:22 -0400

    added another example to the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,24 @@
-module.exports = function(grunt){
-    grunt.registerTask("default", function() {
-        grunt.log.writeln("konnichiwa bitches!")
-    })
-}
+//example 1 Write to console
+// module.exports = function(grunt){
+//     grunt.registerTask("default", function() {
+//         grunt.log.writeln("konnichiwa bitches!")
+//     })
+// }
+//terminal will print the konnichiwa bitches! to the terminal
+
+
+// Example 2 Passing in a parameter
+// module.exports = function(grunt){
+
+//     grunt.registerTask("default", function(name){
+//         grunt.log.writeln("Hello " + name);
+//     })
+
+// }
+
+// So if you passed grunt deafult: "some string"
+// the terminal will return Hello some string
+
+
+
+
