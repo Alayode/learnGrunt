f905469ff50da5972ef48a2dd35753d5811bcd76
Samuel <csamuel@SC07KP1B8DY3H.local>
2016-08-31 12:47:11 -0400

    created intial folders for project : added folders and files to pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,5 +20,33 @@
 // the terminal will return Hello some string
 
 
+// Example 3 start your project with special instructions
+// grunt.config.init
+// module.exports = function(grunt){
+//     grunt.initConfig(grunt.file.readJSON("config.json"))
+
+//     grunt.registerTask("default", function(name){
+//         grunt.log.writeln("Hello, " + grunt.config.get("person").firstName);
+//     })
+// }
+
+
+var colors = require('colors');
+var fs = require('fs');
+var _ = require('lodash');
+
+
+//initial setup read package.json file
+
+module.exports = function (grunt) {
+    grunt.registerTask("default", function () {
+        var defaultConfig = {
+            pkg: grunt.file.readJSON("package.json"),
+
+        }
+    });
+}
+
+
 
 
